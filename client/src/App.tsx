05e7e54504fe5e0a5d5f8d401e8d671c31449246
Benjafo <benjafo@gmail.com>
2025-08-16 16:21:10 -0400

    checkpoint: modify client/src/App.tsx - 16:21

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/blank" element={<BlankPage />} />
             
             <Route element={<ProtectedRoute />}>
               <Route path="/game" element={<GamePage />} />
